6a3dad50e524656ee4e36e416fde00b7245f41ad
Zhanglh8559 <zhanglh_8559@163.com>
2022-03-13 12:15:29 +0800

    zlh添加3个成就

---
+++ citicup/zlh/achievements.py
@@ -0,0 +1,118 @@
+from urllib import request, response
+from django.db import connection
+from django.shortcuts import render
+from django import http
+# Create your views here.
+from django.contrib.auth.models import User, Group
+from django.http import JsonResponse
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.views import APIView
+
+##### 8.餐具收藏家 #####
+def chop_collector(userID:str):
+    res = 0
+    if third_chop(userID) == True:
+        res += 1
+    if second_chop(userID) == True:
+        res += 1
+    if first_chop(userID) == True:
+        res += 1
+    return res #res为1：餐具收藏家（铜），2:银，3:金
+
+# 累计上传50次
+def third_chop(userID:str):
+    cursor = connection.cursor()
+    cursor.execute("select plogName from Plog where plogTypeID=3 and userID=%s",[userID])
+    results = cursor.fetchall()
+    res = False
+
+    if len(results) >= 50:
+        res = True
+    return res
+
+
+# 累计上传100次
+def second_chop(userID:str):
+    cursor = connection.cursor()
+    cursor.execute("select plogName from Plog where plogTypeID=3 and userID=%s",[userID])
+    results = cursor.fetchall()
+    res = False
+
+    if len(results) >= 100:
+        res = True
+    return res
+
+# 累计上传200次
+def first_chop(userID:str):
+    cursor = connection.cursor()
+    cursor.execute("select plogName from Plog where plogTypeID=3 and userID=%s",[userID])
+    results = cursor.fetchall()
+    res = False
+
+    if len(results) >= 200:
+        res = True
+    return res
+
+##### 5.爱心使者 #####
+def clothes(userID:str):
+    res = 0
+    if bronze_clothes(userID) == True:
+        res += 1
+    return res #res为1：爱心使者（金）
+
+# 衣物回收一次 
+def bronze_clothes(userID:str):
+    cursor = connection.cursor()
+    cursor.execute("select plogName from Plog where plogTypeID=5 and userID=%s",[userID])
+    results = cursor.fetchall()
+    res = False
+
+    if len(results) > 0:
+        res = True
+    return res
+
+##### 10.爱心大使 #####
+def clothes_lover(userID:str):
+    res = 0
+    if third_clothes(userID) == True:
+        res += 1
+    if second_clothes(userID) == True:
+        res += 1
+    if first_clothes(userID) == True:
+        res += 1
+    return res #res为1：爱心大使（铜），2:银，3:金
+
+# 衣物回收累计10次
+def third_clothes(userID:str):
+    cursor = connection.cursor()
+    cursor.execute("select plogName from Plog where plogTypeID=5 and userID=%s",[userID])
+    results = cursor.fetchall()
+    res = False
+
+    if len(results) >= 10:
+        res = True
+    return res
+
+
+# 衣物回收累计20次
+def second_clothes(userID:str):
+    cursor = connection.cursor()
+    cursor.execute("select plogName from Plog where plogTypeID=5 and userID=%s",[userID])
+    results = cursor.fetchall()
+    res = False
+
+    if len(results) >= 20:
+        res = True
+    return res
+
+# 衣物回收累计50次
+def first_clothes(userID:str):
+    cursor = connection.cursor()
+    cursor.execute("select plogName from Plog where plogTypeID=5 and userID=%s",[userID])
+    results = cursor.fetchall()
+    res = False
+
+    if len(results) >= 50:
+        res = True
+    return res
