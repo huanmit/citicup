8c497ad4df4d986f53edfddcb6e4f07888470476
lori0423 <83254714+lori0423@users.noreply.github.com>
2022-05-22 14:54:13 +0800

    修改了我的test

--- citicup/lj/tests.py
+++ citicup/lj/tests.py
@@ -1,15 +1,16 @@
 from django.test import TestCase
 import requests
-from django.db import models
+from django.http import JsonResponse
 
 
 class GoodAPIViewTests(TestCase):
-    def setUp(self):
-        class Good(models.Model):
-            question_text = models.CharField(max_length=200)
-            pub_date = models.DateTimeField('date published')
+    def setUp(self):     
         self.base_url = 'http://127.0.0.1:8081/good'
 
     def test_no_goods(self):
-        r = requests.get(self.base_url+'/?=1/')
+        r = requests.get(self.base_url+'/?id=1/')
         self.assertEqual(r.status_code, 500)
+
+    def test_one_goods(self):
+        r = requests.get(self.base_url+'/?id=2/')
+        self.assertEqual(r.status_code, 200)   
